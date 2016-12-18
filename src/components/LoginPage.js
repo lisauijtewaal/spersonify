2d8ef6a5ec28b21576c62e69d378cd27eeb77c8b
Lisa Uijtewaal <development@ongezoet.com>
2016-12-18 18:47:08 +0100

    make redirect variable

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,7 +4,7 @@ class LoginPage extends React.Component {
     render() {
         return (
             <div>
-                <a href="https://accounts.spotify.com/authorize?client_id=2cd7be2114d340bc8014da94c4c45b69&response_type=token&redirect_uri=http://localhost:3000/&scope=user-top-read"
+                <a href={`https://accounts.spotify.com/authorize?client_id=2cd7be2114d340bc8014da94c4c45b69&response_type=token&redirect_uri=${process.env.URI || "http://localhost:3000"}/&scope=user-top-read`}
                    id="login-link">Login met Spotify</a>
             </div>
         )
